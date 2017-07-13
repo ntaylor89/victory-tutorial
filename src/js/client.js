268849d5786576665e7a8b05e147e4ce85c7104b
Nate Taylor <nathantaylor89@gmail.com>
2017-07-12 19:58:41 -0600

    3 - Add your data
    See
    [here](https://formidable.com/open-source/victory/docs/#3-add-your-data)

--- src/js/client.js
+++ src/js/client.js
@@ -2,6 +2,13 @@ import { Component } from 'react'
 import ReactDOM from 'react-dom'
 import * as V from 'victory'
 
+const data = [
+  { quarter: 1, earnings: 13000 },
+  { quarter: 2, earnings: 16500 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 }
+]
+
 class Main extends Component {
   render () {
     return (
